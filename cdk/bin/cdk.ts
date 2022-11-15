e046639f9445075f6f6b7d0de54a12646187ed98
Akash Askoolum <akash1810@gmail.com>
2022-11-15 17:25:32 +0000

    Update lambda configuration
    - Grant SNS publish permission
    - Set environment variables
    
    Co-authored-by: Natasha Thrale <NovemberTang@users.noreply.github.com>

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,10 @@
-import "source-map-support/register";
-import { GuRootExperimental } from "@guardian/cdk/lib/experimental/constructs/root";
-import { SnykTagMonitor } from "../lib/snyk-tag-monitor";
+import 'source-map-support/register';
+import { GuRootExperimental } from '@guardian/cdk/lib/experimental/constructs/root';
+import { SnykTagMonitor } from '../lib/snyk-tag-monitor';
 
 const app = new GuRootExperimental();
-new SnykTagMonitor(app, "SnykTagMonitor-INFRA", { stack: "security", stage: "INFRA", env: {region : "eu-west-1"}});
+new SnykTagMonitor(app, 'SnykTagMonitor-INFRA', {
+	stack: 'security',
+	stage: 'INFRA',
+	env: { region: 'eu-west-1' },
+});
--- cdk/lib/snyk-tag-monitor.ts
+++ cdk/lib/snyk-tag-monitor.ts
@@ -1,41 +1,47 @@
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type {GuScheduledLambdaProps} from "@guardian/cdk/lib/patterns"
-import {GuScheduledLambda} from "@guardian/cdk/lib/patterns"
-import type { App} from "aws-cdk-lib";
-import { Duration } from "aws-cdk-lib";
-import { Schedule } from "aws-cdk-lib/aws-events";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
-import {Secret} from "aws-cdk-lib/aws-secretsmanager";
-import { Topic } from "aws-cdk-lib/aws-sns";
-import { EmailSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
-
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import type { GuScheduledLambdaProps } from '@guardian/cdk/lib/patterns';
+import { GuScheduledLambda } from '@guardian/cdk/lib/patterns';
+import type { App } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import { Schedule } from 'aws-cdk-lib/aws-events';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
 export class SnykTagMonitor extends GuStack {
-  constructor(scope: App, id: string, props: GuStackProps) {
-    super(scope, id, props);
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
 
-    const app = "snyk-tag-monitor"
+		const app = 'snyk-tag-monitor';
 
-    const topic = new Topic(this, `${app}-topic`)
-    topic.addSubscription(new EmailSubscription("devx.security@guardian.co.uk"))
-    const lambdaProps: GuScheduledLambdaProps = {
-      rules: [{ schedule: Schedule.rate(Duration.days(2))}],
-      monitoringConfiguration: {
-        toleratedErrorPercentage: 50,
-        snsTopicName: topic.topicName,
-      },
-      runtime: Runtime.PYTHON_3_9,
-      handler: "main.hello",
-      app,
-      fileName: `${app}.zip` 
-    }
+		const topic = new Topic(this, `${app}-topic`);
+		topic.addSubscription(
+			new EmailSubscription('devx.security@guardian.co.uk'),
+		);
 
-    new GuScheduledLambda(this,
-      app, lambdaProps)
+		const snykApiKey = new Secret(this, 'snykApiKey');
+		const snykGroupId = new Secret(this, 'snykGroupId');
 
-    new Secret(this, "snykApiKey")
-    new Secret(this, "snykGroupId")
+		const lambdaProps: GuScheduledLambdaProps = {
+			rules: [{ schedule: Schedule.rate(Duration.days(2)) }],
+			monitoringConfiguration: {
+				toleratedErrorPercentage: 50,
+				snsTopicName: topic.topicName,
+			},
+			runtime: Runtime.PYTHON_3_9,
+			handler: 'main.handler',
+			app,
+			fileName: `${app}.zip`,
+			environment: {
+				SNYK_GROUP_ID: snykGroupId.secretValue.toString(),
+				SNYK_API_KEY: snykApiKey.secretValue.toString(),
+				SNS_TOPIC_ARN: topic.topicArn,
+			},
+		};
 
-  }
+		const lambda = new GuScheduledLambda(this, app, lambdaProps);
+		topic.grantPublish(lambda);
+	}
 }
