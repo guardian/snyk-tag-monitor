6d9541697b57519dad9947ecafd347caa21751b9
Natasha <natasha.thrale@guardian.co.uk>
2022-11-15 10:17:40 +0000

    simplify ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,20 +5,14 @@ set -x
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR="${DIR}/.."
-cd cdk
-npm ci
-npm run lint
-npm run test
-npm run synth
-npm run build
-cd ..
-
-get_abs_filename() {
-  # $1 : relative filename
-  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
-}
-
-# Create zip files for guardian/actions-riff-raff@v1 GHA
+(
+  cd "${ROOT_DIR}"/cdk
+  npm ci
+  npm run lint
+  npm run test
+  npm run synth
+  npm run build
+)
 
 filename="main.py"
 app_name="snyk-tag-monitor"
