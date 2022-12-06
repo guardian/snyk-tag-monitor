0423f48a53580751f3eae33769cd19ec5646b0f8
Robert Kenny <kenoir@gmail.com>
2022-12-06 12:52:02 +0000

    apply autopep8 changes

--- aws_helpers.py
+++ aws_helpers.py
@@ -6,41 +6,44 @@ import datetime
 _stage = os.environ.get("STAGE", "DEV")
 
 _client_config = Config(
-region_name = 'eu-west-1',
+    region_name='eu-west-1',
 )
+
 
 def _send_tag_count_to_cloudwatch(number_of_tags: int, app_name: str):
 
-    cloudwatch = boto3.client('cloudwatch', config = _client_config)
+    cloudwatch = boto3.client('cloudwatch', config=_client_config)
 
     metric_data = [
         {
             'MetricName': 'snykTagCount',
             'Timestamp': datetime.datetime.now(),
             'Dimensions': [
-            {
-                'Name': 'Stage',
-                'Value': _stage
-            },
-        ],
+                {
+                    'Name': 'Stage',
+                    'Value': _stage
+                },
+            ],
             'Value': number_of_tags,
         },
     ]
 
-    cloudwatch.put_metric_data(Namespace = app_name, MetricData = metric_data)
+    cloudwatch.put_metric_data(Namespace=app_name, MetricData=metric_data)
 
-parameter_store = boto3.client('ssm', config = _client_config)
+
+parameter_store = boto3.client('ssm', config=_client_config)
 
 
 def _send_notification(sns_topic_arn: str, message: str, stage: str):
     if (stage == "INFRA"):
         boto3.client('sns').publish(
-            TargetArn = sns_topic_arn,
-            Message = message,
-            Subject = "Approaching snyk tag limit - action required"
+            TargetArn=sns_topic_arn,
+            Message=message,
+            Subject="Approaching snyk tag limit - action required"
         )
     else:
         print("Local environment detected. Not attempting to publish")
+
 
 def record_tag_count(number_of_tags: int, app_name: str):
     sns_topic_arn: str = os.environ.get("SNS_TOPIC_ARN", None)
@@ -49,8 +52,8 @@ def record_tag_count(number_of_tags: int, app_name: str):
     print("sent tag count to cloudwatch")
     tag_warning_limit = 4500
     tag_hard_limit = 5000
-    
-    if(number_of_tags > tag_warning_limit):
+
+    if (number_of_tags > tag_warning_limit):
         msg = f'There are currently {number_of_tags} Snyk tags. Snyk has a limit of {tag_hard_limit} tags. Go do something about it...'
         _send_notification(sns_topic_arn, msg, _stage)
         print("sent sns notification")
--- snyk_helpers.py
+++ snyk_helpers.py
@@ -2,20 +2,26 @@ from requests import Response
 import requests
 from snyk import SnykClient
 
-def _get_snyk_tag_page(snyk_group_id: str, snyk_api_key: str, page_number: int, page_size: int) -> Response:
-    snykurl: str = "https://api.snyk.io/api/v1/group/" + snyk_group_id + "/tags?perPage=" + str(page_size) + "&page=" + str(page_number)
+
+def _get_snyk_tag_page(snyk_group_id: str, snyk_api_key: str,
+                       page_number: int, page_size: int) -> Response:
+    snykurl: str = "https://api.snyk.io/api/v1/group/" + snyk_group_id + \
+        "/tags?perPage=" + str(page_size) + "&page=" + str(page_number)
     headers: dict[str, str] = {
         'Authorization': f'token {snyk_api_key}',
         'Accept': 'application/json'
     }
     return requests.get(url=snykurl, headers=headers)
 
-def _get_snyk_tags(snyk_group_id: str, snyk_api_key: str, page_number: int, tags: list =[]):
-    page_size: int = 1000
-    response: Response = _get_snyk_tag_page(snyk_group_id, snyk_api_key, page_number, page_size)
 
-    if(response.status_code != 200):
-        print(f'error: failed to get page {page_number} of tags from snyk')
+def _get_snyk_tags(snyk_group_id: str, snyk_api_key: str,
+                   page_number: int, tags: list = []):
+    page_size: int = 1000
+    response: Response = _get_snyk_tag_page(
+        snyk_group_id, snyk_api_key, page_number, page_size)
+
+    if (response.status_code != 200):
+        print(f'Failed to get page {page_number} of tags from snyk')
         print(f'{response.status_code} : {response.content}')
         raise Exception(f'{response.status_code} : {response.content}')
     else:
@@ -23,32 +29,47 @@ def _get_snyk_tags(snyk_group_id: str, snyk_api_key: str, page_number: int, tags
         tags_on_this_page = response_json['tags']
         print(f'page: {page_number}, tags: {len(tags_on_this_page)}')
 
-        if(len(tags_on_this_page) == page_size):
+        if (len(tags_on_this_page) == page_size):
             next_page = page_number + 1
-            return tags + _get_snyk_tags(snyk_group_id, snyk_api_key, next_page, tags_on_this_page)
+            return tags + \
+                _get_snyk_tags(
+                    snyk_group_id,
+                    snyk_api_key,
+                    next_page,
+                    tags_on_this_page)
         else:
             return tags + tags_on_this_page
 
+
 def _delete_tag(tag_dict, group_id, snyk_key):
 
-  delete_tag_url = "https://api.snyk.io/api/v1/group/" + group_id + "/tags/delete"
-  json_body = {"key": tag_dict['key'], "value": tag_dict['value'], "force": False}
+    delete_tag_url = "https://api.snyk.io/api/v1/group/" + group_id + "/tags/delete"
+    json_body = {
+        "key": tag_dict['key'],
+        "value": tag_dict['value'],
+        "force": False}
 
-  response = requests.post(url = delete_tag_url, headers={'Authorization': 'token ' + snyk_key }, json=json_body)
-  if response.status_code != 200:
-    print(f'warning: failed to delete tag, tag: {str(tag_dict)}')
-    print(f'code : {response.status_code}, content: {response.content}')
+    response = requests.post(
+        url=delete_tag_url,
+        headers={
+            'Authorization': 'token ' + snyk_key},
+        json=json_body)
+    if response.status_code != 200:
+        print("Failed to delete tag")
+        print(f'code : {response.status_code}, content: {response.content}')
+
 
 def _get_owned_tags(snyk_key):
 
     def deduplicate_dict_list(dict_list):
         return [dict(t) for t in {tuple(d.items()) for d in dict_list}]
-    
+
     client: SnykClient = SnykClient(snyk_key)
     projects: list = client.projects.all()
     nested_tag_list = [project.tags.all() for project in projects]
     flat_list = [item for sublist in nested_tag_list for item in sublist]
     return deduplicate_dict_list(flat_list)
+
 
 def delete_tags_and_count(snyk_api_key: str, snyk_group_id: str) -> int:
 
@@ -56,13 +77,16 @@ def delete_tags_and_count(snyk_api_key: str, snyk_group_id: str) -> int:
     initial_tag_list = _get_snyk_tags(snyk_group_id, snyk_api_key, 1)
     print(f"starting tags: {len(initial_tag_list)}")
 
-    orphaned_tags = list(filter(lambda x: x not in owned_tags, initial_tag_list))
+    orphaned_tags = list(
+        filter(
+            lambda x: x not in owned_tags,
+            initial_tag_list))
     orphaned_tag_count = len(orphaned_tags)
     print(f"orphaned tags: {orphaned_tag_count}")
 
     for tag in orphaned_tags:
         _delete_tag(tag, snyk_group_id, snyk_api_key)
-    
+
     final_tag_list = _get_snyk_tags(snyk_group_id, snyk_api_key, 1)
 
     number_of_tags = len(final_tag_list)
